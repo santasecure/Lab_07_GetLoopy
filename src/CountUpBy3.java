a0c6c25e364f730b331a11b840743ab798671850
Kristie Santa <k-santa@hotmail.com>
2025-07-04 18:08:43 -0400

    Completed CountUpBy3

--- src/CountUpBy3.java
+++ src/CountUpBy3.java
@@ -1,6 +1,11 @@
 public class CountUpBy3 {
     public static void main(String[] args) {
-        // Loop from 0 to 18 by 3
+        // Pseudocode:
+// START
+//   Create a loop starting at 0 and ending at 18
+//   Increase the counter by 3 each time
+//   Display each number with a space
+// STOP
         for (int i = 0; i <= 18; i += 3) {
             System.out.print(i + " ");
         }
