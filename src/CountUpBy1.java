63c35b5aa9884b5d6297b91a1cf6945d5e10919c
Kristie Santa <k-santa@hotmail.com>
2025-07-04 18:05:48 -0400

    Completed CountDownBy1

--- src/CountUpBy1.java
+++ src/CountUpBy1.java
@@ -1,6 +1,10 @@
 public class CountUpBy1 {
     public static void main(String[] args) {
-        // Loop from 0 to 30 by 1
+        // Pseudocode:
+// START
+//   Create a loop from 0 to 30, stepping by 1
+//   For each number, display it with a space
+// STOP
         for (int i = 0; i <= 30; i++) {
             System.out.print(i + " ");
         }
