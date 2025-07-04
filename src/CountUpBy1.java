bef875ae368271f7b0d990de467502e740f3ad2a
Kristie Santa <k-santa@hotmail.com>
2025-07-04 16:58:29 -0400

    init commit with original code

---
+++ src/CountUpBy1.java
@@ -0,0 +1,10 @@
+public class CountUpBy1 {
+    public static void main(String[] args) {
+        // Loop from 0 to 30 by 1
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
