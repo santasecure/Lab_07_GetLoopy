90cec5b9730af4613628225fe71232731271722b
Kristie Santa <k-santa@hotmail.com>
2025-07-04 17:16:10 -0400

    init commit

---
+++ src/CountUpBy3.java
@@ -0,0 +1,10 @@
+public class CountUpBy3 {
+    public static void main(String[] args) {
+        // Loop from 0 to 18 by 3
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
