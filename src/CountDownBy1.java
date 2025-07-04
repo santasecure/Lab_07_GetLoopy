2786ba441ce12a4d855597aef14dc06bcb1bf89d
Kristie Santa <k-santa@hotmail.com>
2025-07-04 17:06:01 -0400

    init commit

---
+++ src/CountDownBy1.java
@@ -0,0 +1,9 @@
+public class CountDownBy1 {
+    public static void main(String[] args) {
+        // Loop from 30 down to 0 by 1
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
