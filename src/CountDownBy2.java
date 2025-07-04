00ca4751751828fdd4b877c4fdc832aea17cee9e
Kristie Santa <k-santa@hotmail.com>
2025-07-04 18:09:42 -0400

    Completed CountDownBy2

--- src/CountDownBy2.java
+++ src/CountDownBy2.java
@@ -1,6 +1,11 @@
 public class CountDownBy2 {
     public static void main(String[] args) {
-        // Loop from 10 down to 0 by 2
+        // Pseudocode:
+// START
+//   Create a loop starting at 10 and ending at 0
+//   Decrease the counter by 2 each time
+//   Display each number with a space
+// STOP
         for (int i = 10; i >= 0; i -= 2) {
             System.out.print(i + " ");
         }
