a37a4d8b44d795b38622fb5aa6f46c15e412ca22
Kristie Santa <k-santa@hotmail.com>
2025-07-04 17:31:07 -0400

    init commit

---
+++ src/StarSquare.java
@@ -0,0 +1,13 @@
+public class StarSquare {
+    public static void main(String[] args) {
+        // Print 5x5 square of stars
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
