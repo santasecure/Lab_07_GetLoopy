c64620562e66684870f462e63c7c49367bb83e7e
Kristie Santa <k-santa@hotmail.com>
2025-07-04 18:15:37 -0400

    Updated Psuedocode

--- src/CountUpBy1.java
+++ src/CountUpBy1.java
@@ -2,7 +2,7 @@ public class CountUpBy1 {
     public static void main(String[] args) {
         // Pseudocode:
 // START
-//   Create a loop from 0 to 30, stepping by 1
+//   Create a loop, from 0 to 30, stepping by 1
 //   For each number, display it with a space
 // STOP
         for (int i = 0; i <= 30; i++) {
